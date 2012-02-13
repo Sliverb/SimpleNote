863374565752d5ed4b8b369a7676cb49b1d7d468
Bayo O <lilibayo@mit.edu>
2012-02-13 15:59:34 -0800

    Working on Auto Scroll

--- SimpleNote/MainPage.xaml.cs
+++ SimpleNote/MainPage.xaml.cs
@@ -10,15 +10,78 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.IO.IsolatedStorage;
+using System.IO;
 
 namespace SimpleNote
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        bool okToScroll = false;
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
         }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void noteTextBlock_Changed(object sender, TextChangedEventArgs e)
+        {
+            //if (okToScroll && (noteTextBlock.SelectionStart == noteTextBlock.Text.Length))
+            //{
+                noteScrollViewer.ScrollToVerticalOffset(noteTextBlock.ActualHeight);
+            //}
+            //okToScroll = true;
+        }
+
+        private void appBar_Edit(object sender, EventArgs e)
+        {
+
+        }
+
+        private void appBar_Save(object sender, EventArgs e)
+        {
+
+        }
+
+        private void appBar_Delete(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadNote()
+        {
+            var appStorage = IsolatedStorageFile.GetUserStoreForApplication();
+
+            using (var fileStream = appStorage.OpenFile("note.txt", System.IO.FileMode.OpenOrCreate))
+            {
+                using (StreamReader sr = new StreamReader(fileStream))
+                {
+                    noteTextBlock.Text = sr.ReadToEnd();
+                }
+            }
+        }
+
+        private void saveNote()
+        {
+            var appStorage = IsolatedStorageFile.GetUserStoreForApplication();
+
+            using (var fileStream = appStorage.OpenFile("note.txt", FileMode.Create))
+            {
+                using (StreamWriter sw = new StreamWriter(fileStream))
+                {
+                    sw.WriteLine(noteTextBlock.Text);
+                }
+            }
+
+
+        }
+
     }
+
 }
